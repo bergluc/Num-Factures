1dfed8630de312bd6c0511daca17be67a519ee26
Luc Roberge <lroberge@gmail.com>
2020-07-10 09:32:04 -0400

    Update TessOCRtest.ps1
    Modification de certains chemins réseau pour tester avec le répertoire de l'imprimante des achats

--- TessOCRtest.ps1
+++ TessOCRtest.ps1
@@ -1,14 +1,14 @@
-Set-Location 'C:\Users\lroberge\Desktop\FAT'
+Set-Location '\\MFP11222796\FILE_SHARE\002-Facturation-Numériser_Factures'
 
 $TimeStamp = Get-Date -Format yyyyMMdd_HHmmss
-Add-Content -Path 'Transactions.txt' -Value $TimeStamp
-$newdir = 'Historique\' + $TimeStamp 
+Add-Content -Path 'C:\temp\transactions.txt' -Value $TimeStamp
+$newdir = '\historique\' + $TimeStamp 
 New-Item -Path $newdir -ItemType Directory
 
 Get-ChildItem -Filter '*.tif' -Recurse | ForEach-Object {
     $newname = $_.BaseName + $TimeStamp
     C:\Users\lroberge\AppData\Local\Tesseract-OCR\tesseract.exe $_.Name $newname -l fra pdf
-	Add-Content -Path 'C:\Users\lroberge\Desktop\FAT\Transactions.txt' -Value $_.Name
+	Add-Content -Path 'C:\temp\transactions.txt' -Value $_.Name
 	move-item -path $_.Name -destination $newdir 
 }
 
