3c7e66d1075ef8a533a9285d96f1b3146a55a470
Luc Roberge <lroberge@gmail.com>
2020-07-14 16:12:43 -0400

    Add files via upload
    Modifications suite à différent tests, notamment la simplification de la copie et l'utilisation de la variable Fullname

--- TessOCRtest.ps1
+++ TessOCRtest.ps1
@@ -1,6 +1,3 @@
-#Adresse du dossier partagé sur l'imprimante où sont placés les documents numérisés suite à la sélection du modèle créé pour la facturation
-Set-Location '\\MFP11222796\FILE_SHARE\002-Facturation-Numériser_Factures'
-
 #Obtention de la date et de l'heure
 $TimeStamp = Get-Date -Format yyyyMMdd_HHmmss
 
@@ -8,18 +5,24 @@ $TimeStamp = Get-Date -Format yyyyMMdd_HHmmss
 Add-Content -Path 'C:\temp\transactions.txt' -Value $TimeStamp
 
 #Création du répertoire pour l'historique des fichiers traités
-$newdir = 'historique\' + $TimeStamp 
+$newdir = 'C:\temp\historique\' + $TimeStamp 
 New-Item -Path $newdir -ItemType Directory
+
+#À changer par move-item en production --> faut alors enlever le -Recurse?
+copy-item \\mfp11222796\FILE_SHARE\002-Facturation-Numériser_Factures\* -Recurse -destination C:\temp\AT
+
+#Changement du répertoire de traitement
+#Set-Location 'C:\temp\AT'
 
 #Traitement OCR des factures numérisées, journalisation et transfert des fichiers TIF dans l'historique
 Get-ChildItem -Filter '*.tif' -Recurse | ForEach-Object {
     $newname = $_.BaseName + $TimeStamp
-    C:\Users\lroberge\AppData\Local\Tesseract-OCR\tesseract.exe $_.Name $newname -l fra pdf
+    C:\Users\lroberge\AppData\Local\Tesseract-OCR\tesseract.exe $_.FullName $newname -l fra pdf
 	Add-Content -Path 'C:\temp\transactions.txt' -Value $_.Name
-	move-item -path $_.Name -destination $newdir 
+#	move-item -path $_.Name -destination $newdir 
 }
 
 #Transfert des factures PDF dans le répertoire de recherche de la comptabilité
-Get-ChildItem -Filter '*.pdf' | ForEach-Object {
-    move-item -path $_.Name -destination C:\Users\lroberge\Desktop\Index
-}
+#Get-ChildItem -Filter '*.pdf' | ForEach-Object {
+#    move-item -path $_.Name -destination C:\Users\lroberge\Desktop\Index
+#}
