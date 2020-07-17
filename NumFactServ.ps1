c1a32e6d42d5b61d14cd39f095f7d6a8343ad6b0
Luc Roberge <lroberge@gmail.com>
2020-07-17 15:18:55 -0400

    Update NumFactServ.ps1
    Ajout de variables pour simplifier le code et faciliter d'éventuels changements de répertoire

--- NumFactServ.ps1
+++ NumFactServ.ps1
@@ -1,29 +1,36 @@
 #Obtention de la date et de l'heure
 $TimeStamp = Get-Date -Format yyyyMMdd_HHmmss
 $An = Get-Date -Format yyyy
+$Fact = 'E:\Fichiers\Rapports Regionaux\Factures envoyées au siège social\Numérisation\'
+$Mod = '002-Facturation-Numériser_Factures\'
 
 #Ajout de la date/heure dans le fichier de transactions (log)
-Add-Content -Path 'E:\Fichiers\Rapports Regionaux\Factures envoyées au siège social\Numérisation\transactions.txt' -Value $TimeStamp
+Add-Content -Path $Fact + 'transactions.txt' -Value $TimeStamp
+
+#Futur Import=Csv et boucle 
+$Inst = '300'
+$Doss = '\\mfp11222796\FILE_SHARE\'
+$Impr = 'PHOACHAT1'
 
 #Création du répertoire pour l'historique des fichiers traités
-$newdir = 'E:\Fichiers\Rapports Regionaux\Factures envoyées au siège social\Numérisation\historique\' + $TimeStamp 
+$newdir = $Fact + 'historique\' + $TimeStamp +'_' + $Inst
 New-Item -Path $newdir -ItemType Directory
 
 #À changer par move-item en production --> faut alors enlever le -Recurse?
-copy-item \\mfp11222796\FILE_SHARE\002-Facturation-Numériser_Factures\* -Recurse -destination E:\Fichiers\'Rapports Regionaux'\'Factures envoyées au siège social'\Numérisation\AT
-$Source = '300'
+$Source = $Doss + $Mod + '*'
+copy-item $Sources -Recurse -destination $Fact + 'AT'
 
 #Enlever le sous-dossier TestNum en production
-$destdir = 'E:\Fichiers\Rapports Regionaux\Factures envoyées au siège social\TestNum\' + $An + '\' + $Source
+$destdir = $Fact + 'TestNum\' + $An + '\' + $Inst
 
 #Changement du répertoire de traitement
-Set-Location 'E:\Fichiers\Rapports Regionaux\Factures envoyées au siège social\Numérisation\AT'
+Set-Location $Fact + 'AT'
 
 #Traitement OCR des factures numérisées, journalisation et transfert des fichiers TIF dans l'historique
 Get-ChildItem -Filter '*.tif' -Recurse | ForEach-Object {
-    $newname =  $Source + '_' + $TimeStamp + '_' + $_.BaseName
+    $newname =  $Inst + '_' + $Impr + '_' + $TimeStamp + '_' + $_.BaseName
     C:\'Program Files'\Tesseract-OCR\tesseract.exe $_.FullName $newname -l fra pdf
-	Add-Content -Path 'E:\Fichiers\Rapports Regionaux\Factures envoyées au siège social\Numérisation\transactions.txt' -Value $_.Name
+	Add-Content -Path $Fact + 'transactions.txt' -Value $_.Name
 	move-item -path $_.FullName -destination $newdir 
 }
 
