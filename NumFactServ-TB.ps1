f0afea1970618632320a19d2639ac1d8937bb319
Luc Roberge <lroberge@gmail.com>
2020-08-14 13:11:16 -0400

    test de la boucle, ajout de validation

---
+++ NumFactServ-TB.ps1
@@ -0,0 +1,99 @@
+﻿#Obtention de la date et de l'heure
+
+$TimeStamp = Get-Date -Format yyyyMMdd_HHmmssff
+
+$An = Get-Date -Format yyyy
+
+$Fact = '\\cldvsrvfs01\fichiers\Rapports Regionaux\Factures envoyées au siège social\Numérisation\'
+
+$Mod = '002-Facturation-Numériser_Factures\'
+
+
+
+#Ajout de la date/heure dans le fichier de transactions (log)
+
+$Trans = $Fact + 'transactions.txt'
+Add-Content -Path $Trans -Value $TimeStamp
+
+
+
+#Import=Csv et boucle 
+
+$FilePath = "Imprimantes.csv"
+
+$Contenu = Import-CSV $FilePath
+
+
+
+ForEach ($Imprimante in $Contenu) {
+
+$Impr = $($Imprimante.Impr)
+
+$Inst = $($Imprimante.Inst)
+
+$Dossier = $($Imprimante.Dossier)
+
+
+
+#Création du répertoire pour l'historique des fichiers traités
+
+#$newdir = $Fact + 'historique\' + $TimeStamp +'_' + $Inst
+
+#New-Item -Path $newdir -ItemType Directory
+
+
+
+#À changer par move-item en production --> faut alors enlever le -Recurse?
+
+$Source = $Dossier + $Mod + '*'
+$SourDIR = $Dossier + $Mod
+$Message = "Je tente de copier à partir de " + $Source
+Add-Content -Path $Trans -Value $Message
+$AT = $Fact + 'AT'
+If(!(Test-Path $AT)){New-Item -Path $AT -ItemType Directory}
+If(Test-Path $SourDIR){
+	copy-item $Source -Recurse -destination $AT
+
+
+#Enlever le sous-dossier TestNum en production
+
+#$destdir = $Fact + 'TestNum\' + $An + '\' + $Inst
+
+
+
+#Changement du répertoire de traitement
+
+Set-Location $AT
+
+
+
+#Traitement OCR des factures numérisées, journalisation et transfert des fichiers TIF dans l'historique
+
+Get-ChildItem -Filter '*.tif' -Recurse | ForEach-Object {
+
+     $TimeStamp = Get-Date -Format yyyyMMdd_HHmmssff
+     $newname =  $Inst + '_' + $Impr + '_' + $TimeStamp + '_' + $_.BaseName
+
+#    C:\'Program Files'\Tesseract-OCR\tesseract.exe $_.FullName $newname -l fra pdf
+
+	Add-Content -Path $Trans -Value $newname
+#	move-item -path $_.FullName -destination $newdir 
+
+}
+
+
+
+#Transfert des factures PDF dans le répertoire de recherche de la comptabilité
+
+#Get-ChildItem -Filter '*.pdf' | ForEach-Object {
+
+#    move-item -path $_.FullName -destination $destdir
+
+#}
+
+#Suppressions des répertoires après les déplacements de fichiers.
+
+#remove-item -path .\*
+
+}
+}
--- NumFactServ.ps1
+++ NumFactServ.ps1
@@ -1,47 +1,64 @@
 #Obtention de la date et de l'heure
-$TimeStamp = Get-Date -Format yyyyMMdd_HHmmss
+$TimeStamp = Get-Date -Format yyyyMMdd_HHmmssff
 $An = Get-Date -Format yyyy
 $Fact = '\\cldvsrvfs01\fichiers\Rapports Regionaux\Factures envoyées au siège social\Numérisation\'
 $Mod = '002-Facturation-Numériser_Factures\'
 
 #Ajout de la date/heure dans le fichier de transactions (log)
-Add-Content -Path $Fact + 'transactions.txt' -Value $TimeStamp
+$Trans = $Fact + 'transactions.txt'
+Add-Content -Path $Trans -Value $TimeStamp
 
-#Import=Csv et boucle 
+#Importation des imprimantes
+# --- > Ajouter un test : si le fichier n'existe pas, arrêter le script
 $FilePath = "Imprimantes.csv"
 $Contenu = Import-CSV $FilePath
 
+#Traitement des factures sur chaque imprimamte
 ForEach ($Imprimante in $Contenu) {
-$Impr = $($Imprimante.Impr)
-$Inst = $($Imprimante.Inst)
-$Dossier = $($Imprimante.Dossier)
+	$Impr = $($Imprimante.Impr)
+	$Inst = $($Imprimante.Inst)
+	$Dossier = $($Imprimante.Dossier)
 
-#Création du répertoire pour l'historique des fichiers traités
-$newdir = $Fact + 'historique\' + $TimeStamp +'_' + $Inst
-New-Item -Path $newdir -ItemType Directory
+	#Création du dossier AT s'il n'existe pas
+	$AT = $Fact + 'AT'
+	If(!(Test-Path $AT)){New-Item -Path $AT -ItemType Directory}
 
-#À changer par move-item en production --> faut alors enlever le -Recurse?
-$Source = $Dossier + $Mod + '*'
-copy-item $Sources -Recurse -destination $Fact + 'AT'
+	#Initialisation des dossiers pour la copie des factures numérisées
+	$Source = $Dossier + $Mod + '*'
+	$SourDIR = $Dossier + $Mod
 
-#Enlever le sous-dossier TestNum en production
-$destdir = $Fact + 'TestNum\' + $An + '\' + $Inst
+	#Traitement des factures seulement s'il y en a dans le dossier de l'imprimante
+	If(Test-Path $SourDIR){
 
-#Changement du répertoire de traitement
-Set-Location $Fact + 'AT'
+		#Création du répertoire pour l'historique des fichiers traités
+		$newdir = $Fact + 'historique\' + $TimeStamp +'_' + $Inst
+		New-Item -Path $newdir -ItemType Directory
 
-#Traitement OCR des factures numérisées, journalisation et transfert des fichiers TIF dans l'historique
-Get-ChildItem -Filter '*.tif' -Recurse | ForEach-Object {
-    $newname =  $Inst + '_' + $Impr + '_' + $TimeStamp + '_' + $_.BaseName
-    C:\'Program Files'\Tesseract-OCR\tesseract.exe $_.FullName $newname -l fra pdf
-	Add-Content -Path $Fact + 'transactions.txt' -Value $_.Name
-	move-item -path $_.FullName -destination $newdir 
-}
+		#Copie des factures dans le dossier AT (à traiter)
+		# ---> À changer par move-item en production --> faut alors enlever le -Recurse	
+		copy-item $Source -Recurse -destination $AT
 
-#Transfert des factures PDF dans le répertoire de recherche de la comptabilité
-Get-ChildItem -Filter '*.pdf' | ForEach-Object {
-    move-item -path $_.FullName -destination $destdir
-}
-#Suppressions des répertoires après les déplacements de fichiers.
-remove-item -path .\*
+		#Définir le dossier de destination des factures traitées
+		# ---> Enlever le sous-dossier TestNum en production
+		$destdir = $Fact + 'TestNum\' + $An + '\' + $Inst
+
+		#Changement du répertoire de traitement
+		Set-Location $AT
+
+		#Traitement OCR des factures numérisées, journalisation et transfert des fichiers TIF dans l'historique
+		Get-ChildItem -Filter '*.tif' -Recurse | ForEach-Object {
+	    		$newname =  $Inst + '_' + $Impr + '_' + $TimeStamp + '_' + $_.BaseName
+	    		C:\'Program Files'\Tesseract-OCR\tesseract.exe $_.FullName $newname -l fra pdf
+			Add-Content -Path $Trans -Value $_.Name
+			move-item -path $_.FullName -destination $newdir 
+		}
+
+		#Transfert des factures PDF dans le répertoire de recherche de la comptabilité
+		Get-ChildItem -Filter '*.pdf' | ForEach-Object {
+	    		move-item -path $_.FullName -destination $destdir
+		}
+	
+		#Suppressions des répertoires après les déplacements de fichiers.
+		remove-item -path .\*
+	}
 }
